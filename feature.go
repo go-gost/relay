fea5b029f1366bf93e51c420c76d0b1ce8ee905d
ginuerzh <ginuerzh@gmail.com>
2023-01-29 11:42:37 +0800

    add flag for tunnel ID

--- feature.go
+++ feature.go
@@ -272,23 +272,60 @@ func (f *AddrFeature) Decode(b []byte) error {
 	return nil
 }
 
-type TunnelID [16]byte
+type TunnelFlag uint32
+
+const (
+	TunnelPrivate TunnelFlag = 0xffffffff
+)
+
+// TunnelID is an identification for tunnel.
+//
+//	+------------------+
+//	|   ID   |  FLAG   |
+//	+------------------+
+//	|   16   |    4    |
+//	+------------------+
+//
+//	ID: 16-byte tunnel ID value, should be a valid UUID.
+//	FLAG: 4-byte flag, 0xffffffff for private tunnel.
+type TunnelID [20]byte
 
 type ConnectorID = TunnelID
 
 var zeroTunnelID TunnelID
 
+const tunnelIDLen = 16
+
+func NewTunnelID(v []byte) (tid TunnelID) {
+	copy(tid[:tunnelIDLen], v[:])
+	return
+}
+
+func NewPrivateTunnelID(v []byte) (tid TunnelID) {
+	copy(tid[:], v[:])
+	binary.BigEndian.PutUint32(tid[tunnelIDLen:], uint32(TunnelPrivate))
+	return
+}
+
 func (tid TunnelID) IsZero() bool {
-	return tid == zeroTunnelID
+	return bytes.Equal(tid[:tunnelIDLen], zeroTunnelID[:tunnelIDLen])
+}
+
+func (tid TunnelID) IsPrivate() bool {
+	return binary.BigEndian.Uint32(tid[tunnelIDLen:]) == uint32(TunnelPrivate)
+}
+
+func (tid TunnelID) Equal(x TunnelID) bool {
+	return bytes.Equal(tid[:tunnelIDLen], x[:tunnelIDLen])
 }
 
 func (tid TunnelID) String() string {
 	var buf [36]byte
-	encodeHex(buf[:], tid)
+	encodeHex(buf[:], tid[:tunnelIDLen])
 	return string(buf[:])
 }
 
-func encodeHex(dst []byte, v [16]byte) {
+func encodeHex(dst []byte, v []byte) {
 	hex.Encode(dst, v[:4])
 	dst[8] = '-'
 	hex.Encode(dst[9:13], v[4:6])
@@ -321,14 +358,14 @@ func (f *TunnelFeature) Type() FeatureType {
 
 func (f *TunnelFeature) Encode() ([]byte, error) {
 	var buf bytes.Buffer
-	buf.Write(f.ID[:])
+	buf.Write(f.ID[:tunnelIDLen])
 	return buf.Bytes(), nil
 }
 
 func (f *TunnelFeature) Decode(b []byte) error {
-	if len(b) < 16 {
+	if len(b) < tunnelIDLen {
 		return ErrShortBuffer
 	}
-	copy(f.ID[:], b)
+	copy(f.ID[:tunnelIDLen], b)
 	return nil
 }
